377d8868fb9a0e87eb0decd3d41ff03115f4abb0
Sunderous <vivi.orunitia@gmail.com>
2022-05-23 15:27:06 -0300

    Ant nests working

--- MushroomKingdom/Assets/Scripts/AntDamageTrigger.cs
+++ MushroomKingdom/Assets/Scripts/AntDamageTrigger.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AntDamageTrigger : MonoBehaviour
@@ -8,17 +6,8 @@ public class AntDamageTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("An object entered.");
         heroShroom.TakeDamage();
         // TODO: Figure out how to best make this only apply to ants.
         Destroy(other.gameObject); 
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        Debug.Log("An object is still inside of the trigger");
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log("An object left.");
     }
 }
