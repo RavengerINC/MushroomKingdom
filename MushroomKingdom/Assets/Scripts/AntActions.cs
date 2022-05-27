2ffe06cf75d4106ea86a9eb070a5d21e902ea1bf
Sunderous <vivi.orunitia@gmail.com>
2022-05-26 22:34:45 -0300

    Score borked

--- MushroomKingdom/Assets/Scripts/AntActions.cs
+++ MushroomKingdom/Assets/Scripts/AntActions.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AntActions : MonoBehaviour
 {
     [SerializeField] private GameObject deadAntPrefab;
     [SerializeField] private ParticleSystem explosionParticles;
 
+    public UnityEvent AntKilledEvent;
+
+    public void Start() {
+        if(AntKilledEvent == null) {
+            AntKilledEvent = new UnityEvent();
+        }
+    }
+
     public void KillAnt() {
         ParticleSystem ep = Instantiate(explosionParticles, transform.position, Quaternion.identity);
         ep.GetComponent<ParticleSystem>().Play();
         Instantiate(deadAntPrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        AntKilledEvent.Invoke();
     }
 }
---
+++ MushroomKingdom/Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,27 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ScoreTracker : MonoBehaviour
+{
+    public int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AntKilled() {
+        Score += 10;
+        var tmpText = gameObject.GetComponent<TMP_Text>();
+        tmpText.text = Score.ToString();
+    }
+}
